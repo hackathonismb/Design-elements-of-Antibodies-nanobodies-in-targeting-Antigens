24afe37f687f92b2665120f6a956f306a02028ca
Sachendra Kumar <k3sachin@gmail.com>
2021-08-31 23:06:38 +0530

    change in interaction based on sum of interaction and its counts
    # the relative change in interaction based on sum of interactions (Change Hbond +Change Ionic +Change Contact+Change Halogen+Change Pi-Cation+Change Pi-Stacking)

---
+++ molsen_change_sum_interact_count_analysis.py
@@ -0,0 +1,35 @@
+import sys
+import glob
+
+# https://github.com/ncbi/icn3d/tree/master/icn3dnode
+# https://github.com/ncbi/icn3d/blob/master/icn3dnode/interaction2.js
+file1= sys.argv[1] # output from the interaction2.js having the change in the relative interactions due to mutation compared to wild type based on epitopes-paratopes information from PDB
+eph = open(file1.strip(),"r")
+#z=fh.readline()
+
+# the relative change in interaction based on sum of interactions (Change Hbond +Change Ionic +Change Contact+Change Halogen+Change Pi-Cation+Change Pi-Stacking)
+
+count=[]
+epipara=[]
+for line in eph:
+    line=line.strip().split(",")
+    if line[3] == line[4]:
+        a=int(line[5])+int(line[6])+int(line[7])+int(line[8])+int(line[9])+int(line[10])
+        #print("wild"+"="+str(a))
+    if line[3] != line[4]:
+        b=int(line[5])+int(line[6])+int(line[7])+int(line[8])+int(line[9])+int(line[10])
+        #print("mutant"+"="+str(b))
+        if a == b:
+            count.append("no_change"+line[3]+line[2]+line[4])
+        if a > b:
+            count.append("decrease"+line[3]+line[2]+line[4])
+        if a < b:
+            count.append("increase" +line[3]+line[2]+line[4])
+
+def listToString(ls):
+    strg = " "
+    return (strg.join(ls))
+
+# count for the relative change in interaction based on the sum of interactions
+for w in count:
+    print(listToString(w),sep="\n")
