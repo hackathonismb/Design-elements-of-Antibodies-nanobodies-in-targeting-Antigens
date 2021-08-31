54e86e3c13aaf0e97eefa521ecfd4c8257c6fa75
Sachendra Kumar <k3sachin@gmail.com>
2021-08-31 21:05:47 +0530

    Updated code for  insertion, order of input, multiple mutation at same position
    Updated code for the deletion mutation/variants
    Solved order and multiple mutations at same position using unique epitope-paratope position as input

--- molecularsentry_match_mutant_position.py
+++ molecularsentry_match_mutant_position.py
@@ -7,12 +7,17 @@ eph = open(file2.strip(),"r")
 z=fh.readline()
 import itertools
 from itertools import groupby
-from collections import OrderedDict
+#from collections import OrderedDict
+import re
 
 # function to split alpha_numberic characters example ["V141K"] to ["V","141","K"]
-def split_text(alnum):
-    for i, j in groupby(alnum, str.isalpha):
-        yield ''.join(j)
+#def split_text(alnum):
+#    for i, j in groupby(alnum, str.isalpha):
+#       yield ''.join(j)
+
+##updated for the deletion variants example ["V141-"] to ["V","141","-"]
+def split_text(alnumspchr):
+    return filter(None, re.split(r'(\d+)', alnumspchr))
 
 # Processing variants file
 variants=[]
@@ -22,7 +27,8 @@ for line in fh:
     variants.append(line)
     variants_pos.append(list(split_text(line)))
 #print(variants_pos)
-
+# https://github.com/ncbi/icn3d/tree/master/icn3dnode
+# https://github.com/ncbi/icn3d/blob/master/icn3dnode/epitope.js
 # Processing pdb_epitopes_paratopes file from epitope.js command
 
 epipara=[]
@@ -32,6 +38,9 @@ for line in eph:
     pdb=line[1].strip().split("_")
     epipara.append([pdb[0],pdb[1],line[2].strip()])
 #print(epipara)
+
+##updated to get unique epitope-paratope position
+unique_epipara = [list(x) for x in set(tuple(x) for x in epipara)]
 
 # matching amino acid postion for wild type and mutant type
 match_pos_wild=[]
@@ -50,15 +59,10 @@ for i in variants_pos:
 match_pos_wld=(list(itertools.chain.from_iterable(match_pos_wild)))
 match_pos_mut=(list(itertools.chain.from_iterable(match_pos_mutant)))
 
-
-# Preserve the order of the matched postions
-interact_mut=(list(map(list, OrderedDict.fromkeys(map(tuple, match_pos_mut)).keys())))
-interact_wld=(list(map(list, OrderedDict.fromkeys(map(tuple, match_pos_wld)).keys())))
-
 # function for converting list to string as input for interaction2.js command 
 def listToString(ls):
     strg = " "
     return (strg.join(ls))
 
-for w, m in zip(interact_wld, interact_mut):
+for w, m in zip(match_pos_wld, match_pos_mut):
     print((listToString(w)),(listToString(m)), sep="\n")
